a2bf0affe0b3c25a5cb1275a55f62b25d9668f1d
Cauê Castro <cauecastro@outlook.com.br>
2025-03-19 19:34:23 -0300

    AULA 2

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -8,8 +8,8 @@ def hora_para_segundo(valor):
     return valor * 120
 
 def test()
-    assert hora_para_minuto(0) == 0
-    assert hora_para_minuto(1) == 60
+assert hora_para_minuto(0) == 0
+assert hora_para_minuto(1) == 60
     assert hora_para_minuto(2) == 120
 
     assert minuto_para_segundo(0) == 0
