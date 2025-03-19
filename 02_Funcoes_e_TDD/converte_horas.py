1dd39d8aa3ad99d19e3c9dda50f1c1591834aace
Cauê Castro <cauecastro@outlook.com.br>
2025-03-19 20:20:38 -0300

    AULA 2

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -17,7 +17,7 @@ def test()
     assert minuto_para_segundo(2) == 120
 
     assert hora_para_segundo(0) == 0
-    assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
-    assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
+    assert hora_para_segundo(1) == 3600
+    assert hora_para_segundo(2) == 7200
 
 print("Terminou com sucesso!")
