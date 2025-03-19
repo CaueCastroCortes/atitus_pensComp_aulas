97393a63dfdde8b6c13efa241acb21f6b5ba434c
Cauê Castro <cauecastro@outlook.com.br>
2025-03-19 09:33:55 -0300

    Update converte_horas.py

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -26,3 +26,28 @@ assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
 assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
 
 print("Terminou com sucesso!")
+
+def hora_para_minuto(valor):
+    return valor * 60
+    
+
+def minuto_para_segundo(valor):
+    return valor * 60
+
+
+def hora_para_segundo(valor):
+    return valor * 120
+    
+assert hora_para_minuto(0) == 0
+assert hora_para_minuto(1) == 60
+assert hora_para_minuto(2) == 120
+
+assert minuto_para_segundo(0) == 0
+assert minuto_para_segundo(1) == 60
+assert minuto_para_segundo(2) == 120
+
+assert hora_para_segundo(0) == 0
+assert hora_para_segundo(1) == 3600  
+assert hora_para_segundo(2) == 7200
+
+print("Terminou com sucesso!")
