5048230758bcabb47988b6b7643e5da765375968
Cauê Castro <cauecastro@outlook.com.br>
2025-03-19 21:43:56 -0300

    AULA 3

--- 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
+++ 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
@@ -1,9 +1,9 @@
 def raiz_quadrada(valor):
-    return math.sqrt(5,5)
+    return valor ** (1/2)
 
 def test():
     assert raiz_quadrada(9) == 3
     assert raiz_quadrada(16) == 4
     assert raiz_quadrada(25) == 5
 
-print(raiz_quadrada(40))
+print(raiz_quadrada(25))
