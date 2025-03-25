38ad37cb698eeab34d61f8dcbfef1af65c529430
Cauê Castro <cauecastro@outlook.com.br>
2025-03-24 21:59:00 -0300

    AULA 4

--- 04_operadores_logicos_e_condicoes/notas.md
+++ 04_operadores_logicos_e_condicoes/notas.md
@@ -2,6 +2,19 @@ PROGRAMA SE O ANO É BISSEXTO
 
 ano = 1997
 if ano % 4 == 0:
-	print("x é bissexto")
+	print("ano é bissexto")
 else:
 	print("Não é bissexto")
+
+
+def eh_par(valor)
+    if valor % 2 == 0
+        return True
+    else: 
+        return False
+def eh_impar(valor)
+    return not eh_par(valor)
+
+eh_par(10)
+eh_impar(10)
+
--- 04_operadores_logicos_e_condicoes/verifica_positivo.py
+++ 04_operadores_logicos_e_condicoes/verifica_positivo.py
@@ -1,21 +1,26 @@
 def eh_positivo(numero):
-    pass
-
+    if numero > 0
+        return True
 
 def eh_negativo(numero):
-    pass
+    if numero < 0
+        return False
 
 
-assert eh_positivo(1)
-assert eh_positivo(2)
-assert eh_positivo(10)
-assert not eh_positivo(0)
-assert not eh_positivo(-1)
-assert not eh_positivo(-10)
+def test():
+    assert eh_positivo(1)
+    assert eh_positivo(2)
+    assert eh_positivo(10)
+    assert not eh_positivo(0)
+    assert not eh_positivo(-1)
+    assert not eh_positivo(-10)
 
-assert eh_negativo(-1)
-assert eh_negativo(-2)
-assert eh_negativo(-10)
-assert not eh_negativo(0)
-assert not eh_negativo(1)
-assert not eh_negativo(10)
+    assert eh_negativo(-1)
+    assert eh_negativo(-2)
+    assert eh_negativo(-10)
+    assert not eh_negativo(0)
+    assert not eh_negativo(1)
+    assert not eh_negativo(10)
+
+print(eh_negativo)
+print(eh_positivo)
