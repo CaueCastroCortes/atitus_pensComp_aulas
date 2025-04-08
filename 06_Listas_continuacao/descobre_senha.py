1e7f937b388f9c43df62dbcaec25895a864a7bb4
Cauê Castro <cauecastro@outlook.com.br>
2025-04-07 22:09:22 -0300

    AULA 6

---
+++ 06_Listas_continuacao/descobre_senha.py
@@ -0,0 +1,12 @@
+print
+
+numero_secreto = 5
+tentativas = 1 
+palpite = int(input("tente adivinhar o numero:"))
+
+while palpite = numero secreto
+    print("Errado, tente novamente")
+    tentativas += 1
+    palpite = int(input("tente adivinhar o número:"))
+
+print("Parabéns! Você acertou com ", tentativas, "tentativas")
