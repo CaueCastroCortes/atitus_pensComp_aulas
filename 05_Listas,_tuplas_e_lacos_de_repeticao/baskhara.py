044861c86c317931e7ece68c50992fb98884afea
Cauê Castro <cauecastro@outlook.com.br>
2025-04-13 19:02:21 -0300

    AULA 05

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,4 +1,8 @@
 def baskhara(a, b, c):
+import math
+
+
+def baskhara(a, b, c):
     delta = b**2 - 4*a*c
     if delta < 0:
         return None
@@ -12,7 +16,6 @@ def baskhara(a, b, c):
     # retorne apenas um valor se discriminante == 0
     # retorne [x1, x2] nos outros casos
     
-def test()
 
 assert baskhara(1, -3, 2) == [2, 1]
 assert baskhara(2, 3, -2) == [-2, 0.5]
@@ -21,5 +24,3 @@ assert baskhara(1, -7, 10) == [2, 5]
 
 assert baskhara(1, 2, 3) is None
 assert baskhara(1, 0, 0) == 0
-
-print(f"As raízes da equação são: {raizes}")
