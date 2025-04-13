77987dd125b1fdf1fc71c926a7efdcd5649fc08f
Cauê Castro <cauecastro@outlook.com.br>
2025-04-13 18:51:43 -0300

    AULA 5

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,7 +1,7 @@
 def baskhara(a, b, c):
     delta = b**2 - 4*a*c
     if delta < 0:
-        return "A equação não possui raízes reais."
+        return None
 
     x1 = (-b + math.sqrt(delta)) / (2*a)
     x2 = (-b - math.sqrt(delta)) / (2*a)
