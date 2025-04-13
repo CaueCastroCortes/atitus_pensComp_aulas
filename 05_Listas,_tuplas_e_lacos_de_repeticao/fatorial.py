4d790fee322c008fbede1b0609958f66a77b1fbc
Cauê Castro <cauecastro@outlook.com.br>
2025-04-13 19:54:32 -0300

    AULA 05

--- 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
@@ -9,12 +9,12 @@ def fatorial(numero):
             resultado *= i
         return resultado
 
-def test()
+def test():
 
-assert fatorial(0) == 1
-assert fatorial(1) == 1
-assert fatorial(2) == 2
-assert fatorial(3) == 6
-assert fatorial(4) == 24
-assert fatorial(5) == 120
-assert fatorial(-1) is None
+    assert fatorial(0) == 1
+    assert fatorial(1) == 1
+    assert fatorial(2) == 2
+    assert fatorial(3) == 6
+    assert fatorial(4) == 24
+    assert fatorial(5) == 120
+    assert fatorial(-1) is None
