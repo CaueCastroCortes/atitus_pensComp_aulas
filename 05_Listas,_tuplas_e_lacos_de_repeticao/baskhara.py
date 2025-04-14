540bbb6de09740dd18aa83c3adab7bc49650ba45
Cauê Castro <cauecastro@outlook.com.br>
2025-04-14 19:12:49 -0300

    AULA 05

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,12 +1,12 @@
 def baskhara(a, b, c):
     delta = (b**2) - ((4*a)*c)
     if delta < 0:
-        return 
+        return None
     if delta == 0:    
         return x1 or x2
 
-    x1 = (-b + ((b**2 - 4*a*c) * 1/2)) / (2 * a)
-    x2 = (-b - ((b**2 - 4*a*c) * 1/2)) / (2 * a)
+    x1 = (-b + delta**(1/2)) / (2 * a)
+    x2 = (-b - delta**(1/2)) / (2 * a)
     return [x1,x2]
     # retorne None se discriminante < 0
     # retorne apenas um valor se discriminante == 0
