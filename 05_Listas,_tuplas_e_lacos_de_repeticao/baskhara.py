f3407571211f6f5c980e01cf8d517490cadd2958
Cauê Castro <cauecastro@outlook.com.br>
2025-04-14 12:56:43 -0300

    AULA 05

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -14,7 +14,6 @@ def baskhara(a, b, c):
     # retorne [x1, x2] nos outros casos
 
 def test():
-
     assert baskhara(1, -3, 2) == [2, 1]
     assert baskhara(2, 3, -2) == [-2, 0.5]
     assert baskhara(1, -5, 6) == [2, 3]
