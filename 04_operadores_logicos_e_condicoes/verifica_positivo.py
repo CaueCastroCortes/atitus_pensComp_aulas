bfdc0b9f1fcf63a3a70c5ff2b08acb07af76b87e
Cauê Castro <cauecastro@outlook.com.br>
2025-04-18 19:03:24 -0300

    ATUALIZAÇÃO

--- 04_operadores_logicos_e_condicoes/verifica_positivo.py
+++ 04_operadores_logicos_e_condicoes/verifica_positivo.py
@@ -1,13 +1,11 @@
-def eh_positivo(numero):
-    if numero >= 0:
-        return True 
-    else:
-        return False
+def eh_positivo(valor):
+   if valor > 0:
+      return True
+   else:
+      return False
 
-
-def eh_negativo(numero):
-    if not (numero >= 0):
-        return True
+def eh_negativo(valor):
+      return not eh_positivo(valor)
 
 def test():
     
