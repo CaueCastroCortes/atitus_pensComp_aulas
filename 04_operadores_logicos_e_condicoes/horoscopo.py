10aa652f3106e2cdd0e0c59b10a2411adc5d94a4
Cauê Castro <cauecastro@outlook.com.br>
2025-04-18 19:21:56 -0300

    ATUALIZAÇÃO

--- 04_operadores_logicos_e_condicoes/horoscopo.py
+++ 04_operadores_logicos_e_condicoes/horoscopo.py
@@ -1,21 +1,18 @@
-def horoscopo(mes):
-    if mes <= 0:
+def horoscopo(mes): 
+    if mes <= 0 or mes > 12:
         return ("Valor inválido")
     if mes > 0 and mes <= 3:
         return ("Você é do signo de Python")        
-    elif mes > 4 and mes <= 6:
+    if mes > 4 and mes <= 6:
         return ("Você é do signo de Java")
-    elif mes > 7 and mes <= 9:
+    if mes > 7 and mes <= 9:
         return ("Você é do signo de PHP")
-    elif mes > 10 and mes <= 12:
+    if mes > 10 and mes <= 12:
         return ("Você é do signo de TypeScript")   
-    elif mes > 12:
-        return ("Valor inválido")
 
 def test():
-
     assert horoscopo(1) == "Python"
-    assert horoscopo(2) == "Python"
+    assert horoscopo(3) == "Python"
 
     assert horoscopo(4) == "Java"
     assert horoscopo(6) == "Java"
@@ -29,3 +26,6 @@ def test():
     assert horoscopo(-1) is None
     assert horoscopo(0) is None
     assert horoscopo(13) is None
+    
+    
+print (horoscopo(1))
