ac6cdb895f4a6498d01fa83414725d7673358c4b
Cauê Castro <cauecastro@outlook.com.br>
2025-04-18 22:59:20 -0300

    ATUALIZAÇÃO

--- 06_Listas_continuacao/mult_matriz.py
+++ 06_Listas_continuacao/mult_matriz.py
@@ -1,12 +1,18 @@
 def multiply_matrix_by_scalar(matrix, scalar):
-    # Preencher
-    pass
+    resultado = []
 
+    for linha in matrix:
+        result_linha = []
+        for elemento in linha: 
+            result_linha.append(elemento * scalar)
+        resultado.append(result_linha)
+    return resultado 
 
-matrix_1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-result_01 = [[3, 6, 9], [12, 15, 18], [21, 24, 27]]
-assert multiply_matrix_by_scalar(matrix_1, 3) == result_01
+def test():
+    matrix_1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    result_01 = [[3, 6, 9], [12, 15, 18], [21, 24, 27]]
+    assert multiply_matrix_by_scalar(matrix_1, 3) == result_01
 
-matrix_02 = [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
-result_02 = [[8, 12, 16], [20, 24, 28], [32, 36, 40]]
-assert multiply_matrix_by_scalar(matrix_02, 4) == result_02
+    matrix_02 = [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
+    result_02 = [[8, 12, 16], [20, 24, 28], [32, 36, 40]]
+    assert multiply_matrix_by_scalar(matrix_02, 4) == result_02
