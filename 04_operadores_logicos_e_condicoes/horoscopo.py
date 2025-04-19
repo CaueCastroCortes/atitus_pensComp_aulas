276a097a118ad56d2ddc459dbac4c789b97cf4ec
Cauê Castro <cauecastro@outlook.com.br>
2025-04-18 22:30:25 -0300

    ATUALIZAÇÃO

--- 04_operadores_logicos_e_condicoes/horoscopo.py
+++ 04_operadores_logicos_e_condicoes/horoscopo.py
@@ -1,32 +1,28 @@
-def horoscopo(mes):  
+def horoscopo(mes):
     if mes <= 0 or mes > 12:
-        return ("None")
+        return "Valor inválido"
     if mes > 0 and mes <= 3:
-        return ("Você é do signo de Python")        
-    if mes > 4 and mes <= 6:
-        return ("Você é do signo de Java")
-    if mes > 7 and mes <= 9:
-        return ("Você é do signo de PHP")
-    if mes > 10 and mes <= 12:
-        return ("Você é do signo de TypeScript")   
+        return "Você é do signo de Python"        
+    if mes >= 4 and mes <= 6:
+        return "Você é do signo de Java"
+    if mes >= 7 and mes <= 9:
+        return "Você é do signo de PHP"
+    if mes >= 10 and mes <= 12:
+        return "Você é do signo de TypeScript"   
 
 def test():
-    assert horoscopo(1) == "Você é do signo de Python"
-    assert horoscopo(3) == "Você é do signo de Python"
-    
-    assert horoscopo(4) == "Você é do signo de Java"
-    assert horoscopo(6) == "Você é do signo de Java"
-    
-    assert horoscopo(7) == "Você é do signo de PHP"
-    assert horoscopo(9) == "Você é do signo de PHP"
-    
-    assert horoscopo(10) == "Você é do signo de TypeScript"
-    assert horoscopo(12) == "Você é do signo de TypeScript"
-    
-    assert horoscopo(-1) == "Valor inválido"
-    assert horoscopo(0) == "Valor inválido"
-    assert horoscopo(13) == "Valor inválido"
+    assert horoscopo(1) == "Python"
+    assert horoscopo(3) == "Python"
 
-print(horoscopo(12))
-print(horoscopo(-1))
-print(horoscopo(0))
+    assert horoscopo(4) == "Java"
+    assert horoscopo(6) == "Java"
+
+    assert horoscopo(7) == "PHP"
+    assert horoscopo(9) == "PHP"
+
+    assert horoscopo(10) == "TypeScript"
+    assert horoscopo(12) == "TypeScript"
+
+    assert horoscopo(-1) is None
+    assert horoscopo(0) is None
+    assert horoscopo(13) is None
