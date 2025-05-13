30cc7fcd7b5b5b422ccab7e57d5adad46bd98b38
Cauê Castro <cauecastro@outlook.com.br>
2025-05-12 22:06:06 -0300

    AULA 10

--- 10_Introducao_a_funcoes/saudacao.py
+++ 10_Introducao_a_funcoes/saudacao.py
@@ -1,9 +1,23 @@
-def saudacao(nome, sobrenome, titulo):
-    pass
+from typing import Optional
+
+def saudacao(nome, sobrenome, titulo: Optional[str] =  'Sr.')-> None:
+    nome in str:
+
+    if sobrenome is str None:
+        return "nome"
+    else: 
+        return "nome" "sobrenome"
+
+    if titulo is str None:
+        return "nome"
+    else: 
+        return "nome" "sobrenome"
 
 
-assert saudacao("Matheus") == "Olá, Matheus"
-assert saudacao("Matheus", "Jardim") == "Olá, Matheus Jardim"
+
+
+assert saudacao("Matheus") == "Olá, Sr. Matheus"
+assert saudacao("Matheus", "Jardim") == "Olá, Sr. Matheus Jardim"
 assert saudacao("Matheus", "Jardim", "Prof") == "Olá, Prof Matheus Jardim"
 assert saudacao("Matheus", titulo="Prof") == "Olá, Prof Matheus"
 assert saudacao("") == "Olá!"
