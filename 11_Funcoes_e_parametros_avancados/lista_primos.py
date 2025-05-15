74c5a98d401219760c98a361c4a0d4c2daad1820
Cauê Castro <cauecastro@outlook.com.br>
2025-05-14 22:06:08 -0300

    AULA_11.py

--- 11_Funcoes_e_parametros_avancados/lista_primos.py
+++ 11_Funcoes_e_parametros_avancados/lista_primos.py
@@ -7,6 +7,6 @@ def lista_primos(numero):
 
 def test():
 
-assert lista_primos(10) == [2, 3, 5, 7]
-assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
-assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    assert lista_primos(10) == [2, 3, 5, 7]
+    assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
+    assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
--- 11_Funcoes_e_parametros_avancados/primo.py
+++ 11_Funcoes_e_parametros_avancados/primo.py
@@ -9,10 +9,10 @@ def eh_primo(numero: int) -> bool:
 
 def test():
     
-assert not eh_primo(-1)
-assert not eh_primo(0)
-assert not eh_primo(1)
-assert eh_primo(2)
-assert eh_primo(3)
-assert not eh_primo(4)
-assert eh_primo(5)
+    assert not eh_primo(-1)
+    assert not eh_primo(0)
+    assert not eh_primo(1)
+    assert eh_primo(2)
+    assert eh_primo(3)
+    assert not eh_primo(4)
+    assert eh_primo(5)
