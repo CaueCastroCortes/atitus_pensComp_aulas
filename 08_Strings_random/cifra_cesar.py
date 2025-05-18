ab81a7e4b126f49e93679a0b3612a12c555453d0
Cauê Castro <cauecastro@outlook.com.br>
2025-05-18 00:14:17 -0300

    ATUALIZAÇÃO

--- 08_Strings_random/cifra_cesar.py
+++ 08_Strings_random/cifra_cesar.py
@@ -1,11 +1,22 @@
 def caesar_cipher(texto, desvio):
-    pass
+    resultado = []
+    for char in texto:
+        if char.isalpha():
+            if char.isupper():
+                deslocamento = ord('A')  
+            else:
+                deslocamento = ord('a')  
 
+            novo_char = chr((ord(char) - deslocamento + desvio) % 26 + deslocamento)
+            resultado.append(novo_char)
+        else:
+            resultado.append(char)
+    
+    return ''.join(resultado)
 
-assert caesar_cipher("Hello, World!", 3) == "Khoor, Zruog!"
-assert caesar_cipher("Khoor, Zruog!", -3) == "Hello, World!"
-
-assert caesar_cipher("Matheus Jardim", 3) == "Pdwkhxv Mduglp"
-assert caesar_cipher("Pdwkhxv Mduglp", -3) == "Matheus Jardim"
-
-assert caesar_cipher(caesar_cipher("Atitus", 3), -3) == "Atitus"
+# Testes diretos
+print(caesar_cipher("Hello, World!", 3))  # "Khoor, Zruog!"
+print(caesar_cipher("Khoor, Zruog!", -3))  # "Hello, World!"
+print(caesar_cipher("Matheus Jardim", 3))  # "Pdwkhxv Mduglp"
+print(caesar_cipher("Pdwkhxv Mduglp", -3))  # "Matheus Jardim"
+print(caesar_cipher(caesar_cipher("Atitus", 3), -3))  # "Atitus"
