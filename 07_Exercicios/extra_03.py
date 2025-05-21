ff4e639f7b3c4af0fae1418e4b5dc9931d5b2423
Cauê Castro <cauecastro@outlook.com.br>
2025-05-20 22:56:00 -0300

    atualizacao_extra.py

--- 07_Exercicios/extra_03.py
+++ 07_Exercicios/extra_03.py
@@ -1,10 +1,13 @@
 def real_para_dolar(valor, tx_conversao):
-    tx_conversao = 5.20
-    valor = int(input("Digite um valor para realizar a conversao")
-                print (f"A conversão será {valor} / {tx_conversao}")
-  
+    print(f"A conversão será {valor} / {tx_conversao}")
+    return valor / tx_conversao
 
-def test ():
-  assert real_para_dolar(500, 5.20) == 96.23
-  assert real_para_dolar(500, 1) == 500
-  assert real_para_dolar(500, 6) == 83.33333333333333
+  
+def test():
+
+    assert real_para_dolar(500, 5.20) == 500 / 5.20
+    assert real_para_dolar(500, 1) == 500
+    assert real_para_dolar(500, 6) == 500 / 6
+
+
+
