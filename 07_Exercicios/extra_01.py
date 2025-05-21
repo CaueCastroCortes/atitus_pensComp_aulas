389154f9d91bea342d461ea9afa4d1f0ca1e4ed4
Cauê Castro <cauecastro@outlook.com.br>
2025-05-20 23:07:19 -0300

    atualizacao_extra.py

--- 07_Exercicios/extra_01.py
+++ 07_Exercicios/extra_01.py
@@ -35,19 +35,20 @@ def texto_com_letra_upper(texto, letra):
 
 def test():
 
-    assert letra_em_texto("Pensamento Computacional", "a")
-    assert letra_em_texto("Pensamento Computacional", " ")
-    assert not letra_em_texto("Pensamento Computacional", "A")
-    assert not letra_em_texto("Pensamento Computacional", "c")
+  assert letra_em_texto("Pensamento Computacional", "a")
+  assert letra_em_texto("Pensamento Computacional", " ")
+  assert not letra_em_texto("Pensamento Computacional", "A")
+  assert not letra_em_texto("Pensamento Computacional", "c")
 
-    assert conta_letra_em_texto("Pensamento Computacional", "a") == 3
-    assert conta_letra_em_texto("Pensamento Computacional", "A") == 0
-    assert conta_letra_em_texto("Pensamento Computacional", "e") == 2
 
-    assert texto_sem_letra("Pensamento Computacional", "a") == "Pensmento Computcionl"
-    assert texto_sem_letra("Pensamento Computacional", "z") == "Pensamento Computacional"
-    assert texto_sem_letra("Pensamento Computacional", " ") == "PensamentoComputacional"
+  assert conta_letra_em_texto("Pensamento Computacional", "a") == 3
+  assert conta_letra_em_texto("Pensamento Computacional", "A") == 0
+  assert conta_letra_em_texto("Pensamento Computacional", "e") == 2
 
-    assert texto_com_letra_upper("Pensamento Computacional", "a") == "PensAmento ComputAcionAl"
-    assert texto_com_letra_upper("Pensamento Computacional", "z") == "Pensamento Computacional"
-    assert texto_com_letra_upper("Pensamento Computacional", " ") == "Pensamento Computacional"
+  assert texto_sem_letra("Pensamento Computacional", "a") == "Pensmento Computcionl"
+  assert texto_sem_letra("Pensamento Computacional", "z") == "Pensamento Computacional"
+  assert texto_sem_letra("Pensamento Computacional", " ") == "PensamentoComputacional"
+
+  assert texto_com_letra_upper("Pensamento Computacional", "a") == "PensAmento ComputAcionAl"
+  assert texto_com_letra_upper("Pensamento Computacional", "z") == "Pensamento Computacional"
+  assert texto_com_letra_upper("Pensamento Computacional", " ") == "Pensamento Computacional"
