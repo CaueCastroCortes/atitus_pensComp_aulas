ac7cf1d375e534ac1f8570a775b58cfe39250015
Cauê Castro <cauecastro@outlook.com.br>
2025-06-23 17:09:44 -0300

    atualizacao.py

--- 14_manipulacao_de_datas/datas.py
+++ 14_manipulacao_de_datas/datas.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 import calendar
 
+
 def str_to_date(date_str):
     try:
         d = datetime.strptime(date_str, "%d-%m-%Y").date()
@@ -21,6 +22,59 @@ def nome_dia_semana(data):
 def test_nome_dia_semana():
     assert nome_dia_semana(date(year=2025, month=1, day=1)) == 'quarta-feira'
     assert nome_dia_semana(date(year=2025, month=1, day=2)) == 'quinta-feira'
+
+
+
+def dias_para_finde(data):
+    dias_ate_sabado = 5 - data.weekday()
+    return dias_ate_sabado if dias_ate_sabado >= 0 else 0
+
+def test_dias_para_finde():
+    assert dias_para_finde(date(year=2025, month=1, day=1)) == 3
+    assert dias_para_finde(date(year=2025, month=1, day=2)) == 2
+
+
+
+def delta_dias(data_a, data_b):
+    return (data_b - data_a).days if (data_b - data_a).days != 1 else 0
+
+def test_delta_dias():
+    assert delta_dias(date(year=2025, month=1, day=1), date(year=2026, month=1, day=2)) == 365
+    assert delta_dias(date(year=2026, month=1, day=1), date(year=2025, month=1, day=2)) == -365
+    assert delta_dias(date(year=2025, month=1, day=1), date(year=2025, month=1, day=2)) == 0
+
+
+
+def proximo_mes(data_a):
+    ano = data_a.year
+    mes = data_a.month + 1
+    if mes > 12:
+        mes = 1
+        ano += 1
+    ultimo_dia = calendar.monthrange(ano, mes)[1]
+    dia = min(data_a.day, ultimo_dia)
+    return date(ano, mes, dia)
+
+def test_proximo_mes():
+    assert proximo_mes(date(year=2025, month=1, day=1)) == date(year=2025, month=2, day=1)
+    assert proximo_mes(date(year=2025, month=1, day=29)) == date(year=2025, month=2, day=28)
+    assert proximo_mes(date(year=2024, month=1, day=29)) == date(year=2024, month=2, day=29)
+    assert proximo_mes(date(year=2025, month=1, day=30)) == date(year=2025, month=2, day=28)
+
+def data_futuro(data: date) -> int:
+    hoje = date.today()
+    if data > hoje:
+        return 1
+    elif data < hoje:
+        return -1
+    else:
+        return 0
+
+def test_data_futuro():
+    assert data_futuro(date(day=1, month=1, year=2099)) == 1
+    assert data_futuro(date(day=1, month=1, year=1999)) == -1
+    assert data_futuro(date.today()) == 0
+
 
 
 
