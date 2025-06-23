860dd999c78b10fa270ff6c21ce92ad87b12d625
Cauê Castro <cauecastro@outlook.com.br>
2025-06-23 16:34:55 -0300

    print_calendario.py

--- 13_exercicios_leetcode/print_calendario.py
+++ 13_exercicios_leetcode/print_calendario.py
@@ -1,41 +1,48 @@
 def imprimir_calendario_mes(dia_inicial: int, total_dias: int):
-    dias_da_semana = "Do.Se.Te.Qu.Qu.Se.Sá"
-    calendario = [dias_da_semana]
-    
-    linha = ['.'] * 42 
-    
-    for dia in range(1, total_dias + 1):
-        linha[dia_inicial + dia - 1] = str(dia)
-    
-    for i in range(6):
-        calendario.append(''.join(linha[i * 7:(i + 1) * 7]).rstrip('.'))
-    
+    cabecalho = "Do.Se.Te.Qu.Qu.Se.Sá"
+    calendario = [cabecalho]
+
+    linha = "." * (dia_inicial * 3)
+    dia = 1
+
+    while dia <= total_dias:
+        while len(linha) < 20 and dia <= total_dias:
+            if len(linha) > 0 and linha[-1] != '.':
+                linha += '.'
+            if dia < 10:
+                linha += f"{dia}."
+            else:
+                linha += f"{dia}."
+            dia += 1
+        calendario.append(linha.rstrip('.'))
+        linha = ""
+
     return calendario
 
-def test():
+def test_imprimir_calendario_mes():
     assert imprimir_calendario_mes(0, 31) == [
-    "Do.Se.Te.Qu.Qu.Se.Sá",
-    ".1..2..3..4..5..6..7",
-    ".8..9.10.11.12.13.14",
-    "15.16.17.18.19.20.21",
-    "22.23.24.25.26.27.28",
-    "29.30.31",
-]
+        "Do.Se.Te.Qu.Qu.Se.Sá",
+        ".1..2..3..4..5..6..7",
+        ".8..9.10.11.12.13.14",
+        "15.16.17.18.19.20.21",
+        "22.23.24.25.26.27.28",
+        "29.30.31",
+    ]
 
     assert imprimir_calendario_mes(1, 31) == [
-    "Do.Se.Te.Qu.Qu.Se.Sá"
-    "....1..2..3..4..5..6"
-    ".7..8..9.10.11.12.13"
-    "14.15.16.17.18.19.20"
-    "21.22.23.24.25.26.27"
-    "28.29.30.31"
-]
+        "Do.Se.Te.Qu.Qu.Se.Sá",
+        "....1..2..3..4..5..6",
+        ".7..8..9.10.11.12.13",
+        "14.15.16.17.18.19.20",
+        "21.22.23.24.25.26.27",
+        "28.29.30.31",
+    ]
 
     assert imprimir_calendario_mes(2, 31) == [
-    "Do.Se.Te.Qu.Qu.Se.Sá"
-    ".......1..2..3..4..5"
-    ".6..7..8..9.10.11.12"
-    "13.14.15.16.17.18.19"
-    "20.21.22.23.24.25.26"
-    "27.28.29.30.31"
-]
+        "Do.Se.Te.Qu.Qu.Se.Sá",
+        ".......1..2..3..4..5",
+        ".6..7..8..9.10.11.12",
+        "13.14.15.16.17.18.19",
+        "20.21.22.23.24.25.26",
+        "27.28.29.30.31",
+    ]
